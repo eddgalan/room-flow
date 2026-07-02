522624a026202eb4e8aa36471ca13d0a55272cdc
Edson Galan <edd.galan@hotmail.com>
2026-07-02 01:32:03 -0600

    #2 Add web app manifest and update favicon - Update app logo - Update icon components with new Room Flow branding and dynamic light/dark mode support

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,9 @@
 
         <link rel="icon" href="/favicon.ico" sizes="any">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
+        <link rel="icon" href="/favicon-96x96.png" type="image/png" sizes="96x96">
         <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="manifest" href="/site.webmanifest">
 
         @fonts
 
