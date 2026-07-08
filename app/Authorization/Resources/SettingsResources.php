3497803a63dfb2ccb57eb00e98c358246bc13423
Edson Galan <edd.galan@hotmail.com>
2026-07-07 20:15:39 -0600

    #4 Add SettingsResources to define actions and permissions using constants

---
+++ app/Authorization/Resources/SettingsResources.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Authorization\Resources;
+
+/**
+ * Define all the actions, permissions, and resources available on the settings section using constants
+ */
+class SettingsResources implements ResourceDefinition
+{
+    public const string PROFILE = 'settings.profile.view';
+    public const string PROFILE_UPDATE = 'settings.profile.update';
+    public const string ROLES = 'settings.roles.view';
+    public const string SECURITY = 'settings.security';
+    public const string APPEARANCE = 'settings.appearance';
+}
